50dad904a25f9851c0297071e98771afb7137e61
Kristian Randall <kristian.l.randall@gmail.com>
2020-06-01 01:30:54 +0700

    Add document title

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import DocumentTitle from 'react-document-title'
 import { PlayBar, ModePicker, Mode, Player } from '../../components'
 import { videoIds, getCurrentHour } from '../../utils/hourly'
 import classes from './Main.module.css'
@@ -25,18 +26,20 @@ const Main = () => {
   const onPlayClick = () => setPaused(prev => !prev)
 
   return (
-    <>
-      <h1 className={classes.title}>
-        Animal Crossing Radio
-      </h1>
-      <div className={classes.controls}>
-        <ModePicker value={mode} onChange={event => setMode(event.target.value)} />
-      </div>
-      <main className={classes.main}>
-        <PlayBar hour={hour} mode={mode} onPlayClick={onPlayClick} setHour={setHour} paused={paused} />
-        <Player videoId={videoIds[hour]} paused={paused} />
-      </main>
-    </>
+    <DocumentTitle title={`${hour} - Animal Crossing Radio`}>
+      <>
+        <h1 className={classes.title}>
+          Animal Crossing Radio
+        </h1>
+        <div className={classes.controls}>
+          <ModePicker value={mode} onChange={event => setMode(event.target.value)} />
+        </div>
+        <main className={classes.main}>
+          <PlayBar hour={hour} mode={mode} onPlayClick={onPlayClick} setHour={setHour} paused={paused} />
+          <Player videoId={videoIds[hour]} paused={paused} />
+        </main>
+      </>
+    </DocumentTitle>
   );
 }
 
